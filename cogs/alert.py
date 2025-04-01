8a2726838b6e643d4c764e1c724780bf6c98d53e
osqwety <lamborgini8032@gmail.com>
2025-04-01 09:56:46 -0500

    did smth#

--- cogs/alert.py
+++ cogs/alert.py
@@ -18,8 +18,6 @@ class alert(commands.Cog):
             ch2 = await member.create_dm()
             await ctx.reply("Alert Succesful!")
         else:
-            #await member.send(f"ALERT!! {member.mention} '{msg}' BY {ctx.author.name}")
-            #print(x)
             await ctx.reply("Alert Succesful!")
         for x in range(int(ts)):
             await member.send(f"ALERT!! {member.mention} '{msg}' BY {ctx.author.name}")
@@ -32,10 +30,6 @@ class alert(commands.Cog):
         global x
         x = 1 + int(ts1)
         await ctx.send("Cancelled")
-            #if msg.content == "acancel":
-             #   break
-            #if self.client.unload_extension():
-             #   break
 
 async def setup(bot):
     await bot.add_cog(alert(bot))
